7608ce2338dd8c8f2b5149766cac170e5b8e9901
Mark T. Holder <mtholder@gmail.com>
2023-06-14 14:36:33 -0500

    batch_download.py

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # avian-influenza-analyses
 
 Large datafiles will be stored in full_data and not synced through git.
+
+
+# MTH notes
+## env setup 
+I'm using virtualenv:
+
+    python3 -mvenv env
+    source env/bin/activate
+    pip install biopython
+
+Note that you'll have to run 
+
+	source env/bin/activate
+
+## Fetching data
+
+   1. Configure and add a search query on https://www.ncbi.nlm.nih.gov/genomes/FLU/Database/nph-select.cgi
+   2. Select "Nucletide accesion list" from the drop-down next to the "Download results"
+   3. store the downloaded file as `full_data/{genename}/accessions.fa`
+   4. https://www.ncbi.nlm.nih.gov/sites/batchentrez
---
+++ bin/batch_download.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Based on code from
+  https://www.biostars.org/p/66921/#188448 by Eli Korvigo
+"""
+import sys
+from Bio import Entrez
+import os
+import time
+
+entrez_email = os.environ.get("ENTREZ_EMAIL")
+if entrez_email:
+    Entrez.email = entrez_email
+
+RETMAX = 10 ** 9
+
+
+def batch_query(accessions_batch, db="nucleotide", retmax=RETMAX, batchsize=500):
+    # get GI for query accessions
+    query = " ".join(accessions_batch)
+    query_handle = Entrez.esearch(db=db, term=query, retmax=retmax)
+    gi_list = Entrez.read(query_handle)["IdList"]
+
+    # get GB files
+    search_handle = Entrez.epost(db=db, id=",".join(gi_list))
+    search_results = Entrez.read(search_handle)
+    webenv, query_key = search_results["WebEnv"], search_results["QueryKey"]
+    records_handle = Entrez.efetch(
+        db=db, rettype="gb", retmax=batchsize, webenv=webenv, query_key=query_key
+    )
+    return records_handle
+
+
+def batch(accessions, size=500):
+    l = len(accessions)
+    for start in range(0, l, size):
+        yield accessions[start : min(start + size, l)]
+
+
+def main(acc_fp):
+    batchsize = 500
+    out = sys.stdout
+    err = sys.stderr
+    verbose = True
+
+    acc_list = []
+    with open(acc_fp, "r") as inp:
+        tmp = [i.strip() for i in inp]
+        acc_list = [i for i in tmp if i]  # skip empty lines
+    if not acc_list:
+        raise RuntimeError("No accessions found")
+    rec_ind = 1
+
+    for abatch in batch(acc_list, size=batchsize):
+        if rec_ind > 1:
+            time.sleep(20)
+        if verbose:
+            err.write(f"Fetching records {rec_ind} to {len(abatch) + rec_ind}\n")
+        rec_ind += len(abatch)
+        records = batch_query(abatch, batchsize=batchsize)
+        for line in records:
+            out.write(line)
+
+
+if __name__ == "__main__":
+    try:
+        acc_fp = sys.argv[1]
+    except:
+        sys.exit("Expected 1 argument: a filepath to a list of accession numbers\n")
+    main(acc_fp)
